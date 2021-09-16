97e4be2124c2d5f8ea3fa9c23a1efc3cd3ca0e07
sz0grikri <sz0grikri@vasvari.org>
2021-09-16 12:13:18 +0200

    asd

---
+++ SpectatorProject/SpectatorProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SpectatorProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("H3llo World!");
+        }
+
+    }
+}
