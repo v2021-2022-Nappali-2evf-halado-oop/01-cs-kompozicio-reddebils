2f025c3fd2a5e68f7964c73124cb2fd96e7f51b9
sz0grikri <sz0grikri@vasvari.org>
2021-09-16 12:27:20 +0200

    Státusz enum kész

---
+++ SpectatorProject/SpectatorProject/Model/Name.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpectatorProject.Model
+{
+    class Name
+    {
+        enum Status
+        {
+            Inactive,
+            Active,
+            Expired
+        }
+
+
+    }
+}
