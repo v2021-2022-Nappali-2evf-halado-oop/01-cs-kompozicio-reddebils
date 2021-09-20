60a4657a9a7429017632cc943db2fe62a0353767
sz0grikri <sz0grikri@vasvari.org>
2021-09-20 12:00:07 +0200

    Main programban tesztkód

--- SpectatorProject/SpectatorProject/Program.cs
+++ SpectatorProject/SpectatorProject/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SpectatorProject.Model;
+using System;
+using System.Globalization;
+using System.Threading;
 
 namespace SpectatorProject
 {
@@ -6,7 +9,29 @@ namespace SpectatorProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("H3llo World!");
+            Model.Name name = new Name("Látó", "László");
+            Console.WriteLine("Teljes név tulajdonság: " + name.Fullname);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("hu-HU");
+            DateTime dt = new DateTime(2022, 3, 15);
+            string dateString = dt.ToString();
+            CreditCard creditCard = new CreditCard(dt, "11");
+            if (creditCard.Expired)
+            {
+                Console.WriteLine("Lejárat tulajdonság: Lejárt.");
+            }
+            else
+            {
+                Console.WriteLine("Lejárat tulajdonság: Érvényes.");
+            }
+            Status status = Status.Active;
+            BonusCard bonusCard = new BonusCard(30000, "12", status);
+            Spectator viewer = new Spectator(name, creditCard, bonusCard);
+            if (bonusCard.Usable)
+            {
+                Console.WriteLine("A bonuszkártya használható:");
+            }
+
+            Console.WriteLine(viewer);
         }
 
     }
